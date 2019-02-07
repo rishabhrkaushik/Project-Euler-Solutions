2ca2631ed36aa3070f8df6f4405d79ae7b3732ee
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2019-02-07 17:04:05 +0530

    Update p001.py
    added comments

--- p001.py
+++ p001.py
@@ -10,9 +10,12 @@ import sys
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
-    sum = 0
+    # used sum of AP to make the problem O(1)
+    # sum of AP = n/2 * [2*a+(n−1) * d]  where n is number of terms, a is first term, d is difference between 2 terms
+    # n3 = number of terms in AP with d = 3
+    # sum3 = sum of AP
     n3 = (n-1)//3
-    sum3 = n3 * (6 + ((n3-1) * 3))
+    sum3 = n3 * (6 + ((n3-1) * 3)) 
     n5 = (n-1)//5
     sum5 = n5 * (10 + ((n5-1) * 5))
     n15 = (n-1)//15
