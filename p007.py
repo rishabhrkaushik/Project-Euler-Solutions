b440f54005c74b0f682edf122f3e32f17d46e928
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2019-06-04 15:41:23 +0530

    Create p007.py
    Find nth prime number

---
+++ p007.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+"""
+https://www.hackerrank.com/contests/projecteuler/challenges/euler007/problem
+"""
+
+import sys
+
+
+t = int(input().strip())
+
+allN = []
+for a0 in range(t):
+    n = int(input().strip())
+    # add all n to a list to find m prime numbers where m is largest n required
+    allN.append(n) 
+
+allPrimes = [2]
+currentNumber = 3
+
+# check if num is divisible with any prime number before it
+def isPrime(num, allPrimes):
+    #print(num, allPrimes)
+    for prime in allPrimes:
+        #print(prime)
+        if (num % prime == 0):
+            #print(False)
+            return False 
+    return True
+
+# add first m prime number to a list
+while(len(allPrimes) < max(allN)):
+    # append to list if the number is prime
+    if(isPrime(currentNumber, allPrimes)):
+        allPrimes.append(currentNumber)
+    currentNumber += 2
+
+#print(allPrimes)
+for num in allN:
+    print(allPrimes[num - 1])
