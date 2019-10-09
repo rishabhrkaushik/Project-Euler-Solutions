bac1a880f2152446d0358ae64ca234007c60955f
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2019-10-10 01:49:22 +0530

    Update p012.py
    passing all the test cases

--- p012.py
+++ p012.py
@@ -7,10 +7,12 @@
     Purpose: This is part of the series to the solution to Project Euler.
     Problem Statement: https://www.hackerrank.com/contests/projecteuler/challenges/euler012/problem
 """
+
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 import math
   
-MAXN = 1000000
+# maximum number till where primes needs to be checked
+MAXN = 100000
   
 # array specifying if the below index is prime or not
 isPrime = [ True ] * MAXN
@@ -24,12 +26,10 @@ for i in range(2, int(math.sqrt(MAXN))):
 isPrime[0] = False
 isPrime[1] = False
 
+# list of all primes
 primes = [i for i, x in enumerate(isPrime) if x == True]
-# print(primes)
 
-# A O(log n) function returning prime  
-# factorization by dividing by smallest  
-# prime factor at every step 
+# find prime factors of number
 def primeFactors(x): 
     factors = []
     i = 0
@@ -41,24 +41,33 @@ def primeFactors(x):
             i = i + 1
     return factors
 
-# print(primeFactors(100))
-
-# n = 4
+# save number of divisors to prevent recalculation
+divisors = {}
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
     i = 2
     found = False
     while(not found):
+        # nth triangle number is sum of first n natural numbers
+        # using sum of AP to find the number        
         triangle = int(((i) * (i + 1))/2)
-    #     print("Triangle: ", triangle)
-        factors = primeFactors(triangle)
-    #     print(factors)
-        tempN = 1
-        for unique in set(factors):
-            tempN = tempN * (factors.count(unique) + 1)
-    #     print(tempN)
-        if(tempN > n):
+        # check if number of divisors have already been calculated        
+        try:
+            divisors[i]
+        except:
+            # find prime factors for the number
+            factors = primeFactors(triangle)
+            # number of divisors
+            tempN = 1
+            # if the prime factors of a number is a1^b1 , a2^b2 , a3^b3 ..., an ^ bn then the number of divisors is (b1 + 1)(b2 + 1)(b3 + 1)...(bn + 1)
+            for unique in set(factors):
+                tempN = tempN * (factors.count(unique) + 1)
+            # add it to dictionary
+            divisors[i] = tempN
+        # check if minimum number of divisors have been matched
+        if(divisors[i] > n):
             found = True
             print(triangle)
         i = i + 1
+
